3cdb1297801bb847f1186e6e3193d92bf4a122c8
Pavel Shuvalov <shuvalov.pavel@gmail.com>
2023-02-13 16:28:28 +0300

    Gate io

--- etl/dags/gate_io_exchange.py
+++ etl/dags/gate_io_exchange.py
@@ -15,15 +15,15 @@ import requests
     catchup=False,
     concurrency=1,
     max_active_runs=1,
-    tags=['ton', 'jettons', 'datamart', 'mvp', 'cex', 'mexc']
+    tags=['ton', 'jettons', 'datamart', 'mvp', 'cex', 'gate.io']
 )
-def mexc_fetcher():
+def gateio_fetcher():
     create_tables = PostgresOperator(
         task_id="create_tables",
         postgres_conn_id="ton_db",
         sql=[
             """
-        CREATE TABLE IF NOT EXISTS mexc_stat (
+        CREATE TABLE IF NOT EXISTS gateio_stat (
             id bigserial NOT NULL primary key,
             check_time timestamp with time zone NOT NULL, 
             address varchar,
@@ -32,34 +32,32 @@ def mexc_fetcher():
             market_volume_ton_24 decimal(40, 0)                              
         );""",
             """
-        CREATE INDEX IF NOT EXISTS mexc_stat_idx
-        ON mexc_stat (address, check_time DESC);            
+        CREATE INDEX IF NOT EXISTS gateio_stat_idx
+        ON gateio_stat (address, check_time DESC);            
             """
         ]
     )
 
 
-    def fetch_info_mexc():
+    def fetch_info_gateio():
         postgres_hook = PostgresHook(postgres_conn_id="ton_db")
-        to_insert = []
-        now = int(time())
-        fnz = requests.get("https://api.mexc.com/api/v3/ticker/24hr?symbol=FNZUSDT").json()
-        usdt = requests.get("https://api.mexc.com/api/v3/ticker/24hr?symbol=TONUSDT").json()
-        fnz_ton_volume = round(float(fnz['quoteVolume']) / float(usdt['lastPrice']))
-        fnz_ton_price = float(fnz['lastPrice']) / float(usdt['lastPrice'])
-        insert_sql = f"""INSERT INTO mexc_stat(address, check_time, symbol,
+        fnz = requests.get("https://api.gateio.ws/api/v4/spot/tickers?currency_pair=FNZ_USDT").json()[0]
+        usdt = requests.get("https://api.gateio.ws/api/v4/spot/tickers?currency_pair=TONCOIN_USDT").json()[0]
+        fnz_ton_volume = round(float(fnz['quote_volume']) / float(usdt['last']))
+        fnz_ton_price = float(fnz['last']) / float(usdt['last'])
+        insert_sql = f"""INSERT INTO gateio_stat(address, check_time, symbol,
         price, market_volume_ton_24 )
          VALUES ('EQDCJL0iQHofcBBvFBHdVG233Ri2V4kCNFgfRT-gqAd3Oc86', now(), 'FUNZ', 
          {fnz_ton_price}, {fnz_ton_volume});
         """
         postgres_hook.run(insert_sql, autocommit=True)
 
-    fetch_info_mexc_task = PythonOperator(
-        task_id=f'fetch_info_mexc',
-        python_callable=fetch_info_mexc
+    fetch_info_gateio_task = PythonOperator(
+        task_id=f'fetch_info_gateio',
+        python_callable=fetch_info_gateio
     )
 
-    create_tables >> fetch_info_mexc_task
+    create_tables >> fetch_info_gateio_task
 
 
-mexc_fetcher_dag = mexc_fetcher()
+gateio_fetcher_dag = gateio_fetcher()
