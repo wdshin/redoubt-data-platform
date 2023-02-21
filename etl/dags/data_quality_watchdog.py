e18ef8b2f0610ae7a10bbfb9eb6878a6f2a527df
Pavel Shuvalov <shuvalov.pavel@gmail.com>
2023-02-21 14:30:37 +0300

    Add data quality alerts

--- etl/dags/data_quality_watchdog.py
+++ etl/dags/data_quality_watchdog.py
@@ -1,12 +1,76 @@
+import dataclasses
 from airflow.decorators import dag, task
 from airflow.operators.python import PythonOperator
 from airflow.providers.telegram.hooks.telegram import TelegramHook
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 from datetime import datetime
-from time import time
 import logging
 import requests
 
+@dataclasses.dataclass
+class Check:
+    name: str
+    sql: str
+    limit: int
+    condition: str = "<"  # < or >
+
+CHECKS = [
+    Check(
+        name='Gate.io lag',
+        sql="select round(extract(epoch from now() - max(check_time))) as lag from gateio_stat",
+        limit=400
+    ),
+    Check(
+        name='MEXC lag',
+        sql="select round(extract(epoch from now() - max(check_time))) as lag from mexc_stat",
+        limit=400
+    ),
+    Check(
+        name='TON Rocket lag',
+        sql="select round(extract(epoch from now() - max(check_time))) as lag from ton_rocket_stat",
+        limit=400
+    ),
+    Check(
+        name='Top jetton datamart lag',
+        sql="select round(extract(epoch from now() - max(build_time))) as lag from top_jettons_datamart",
+        limit=1800
+    ),
+    Check(
+        name='Last DEX swap time',
+        sql="select round(extract(epoch from now() - max(swap_time))) as lag from mview_dex_swaps",
+        limit=3600
+    ),
+    Check(
+        name='Last block from blockchain',
+        sql="select round(extract(epoch from now() - to_timestamp(max(utime)))) as lag from transactions",
+        limit=60
+    ),
+    Check(
+        name='Jetton operations',
+        sql="""
+        with jetton_actions as (
+            select max(utime) as utime from jetton_transfers
+            union all
+            select max(utime) as utime from jetton_burn
+            union all
+            select max(utime) as utime from jetton_mint
+        )
+        select round(extract(epoch from now() - to_timestamp(max(utime)))) as lag from jetton_actions
+        """,
+        limit=1800
+    ),
+    Check(
+        name='Parse queue size',
+        sql="select count(1) from parse_outbox",
+        limit=3000
+    ),
+    Check(
+        name='Unparsed accounts size',
+        sql="select count(1) from accounts where last_check_time is null",
+        limit=200
+    )
+]
 
 @dag(
     schedule_interval="*/30 * * * *",
@@ -42,7 +106,21 @@ def data_quality_watchdog():
         python_callable=check_jetton_api
     )
 
-    [check_jetton_api]
+    def check_datamart_lag():
+        postgres_hook = PostgresHook(postgres_conn_id="ton_db")
+        for check in CHECKS:
+            value = postgres_hook.get_first(check.sql)[0]
+            if (check.condition == "<" and value > check.limit) or (check.condition == ">" and value < check.limit):
+                send(f"🕗 {check.name}: {value} (limit: {check.limit})")
+
+
+    check_datamart_lag_task = PythonOperator(
+        task_id=f'check_datamart_lag',
+        python_callable=check_datamart_lag
+    )
+
+
+    check_jetton_api_task >> check_datamart_lag_task
 
 
 data_quality_watchdog_dag = data_quality_watchdog()
