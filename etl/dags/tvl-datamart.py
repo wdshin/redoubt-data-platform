5640bfc44ea77de5b48f6d6776143ebf0f0f8e75
Pavel Shuvalov <shuvalov.pavel@gmail.com>
2023-02-26 20:05:57 +0300

    Add TVL calculation in TON

--- etl/dags/tvl-datamart.py
+++ etl/dags/tvl-datamart.py
@@ -25,7 +25,17 @@ def tvl_datamart():
             );""",
             """
         create unique index if not exists lp_info_idx1 on lp_info(address);            
+            """,
             """
+        CREATE TABLE IF NOT EXISTS tvl_history_datamart (
+            id bigserial NOT NULL primary key,
+            build_time timestamp with time zone NOT NULL,
+            platform varchar, 
+            address varchar,
+            jetton_a varchar,
+            jetton_b varchar,
+            tvl_ton decimal(20, 0)                              
+        );""",
         ]
     )
 
@@ -112,7 +122,7 @@ def tvl_datamart():
               left join current_balances_ton cbt on cbt.address = pools.address
               where jetton_a is not null and jetton_b is not null
             )
-            select * from pools_with_balances where balance_a > 0 and balance_b > 0
+            select distinct * from pools_with_balances where balance_a > 0 and balance_b > 0
             """,
             """
             create unique index if not exists mview_dex_pools_balances_address on mview_dex_pools_balances(address);            
@@ -123,7 +133,76 @@ def tvl_datamart():
         ]
     )
 
-    create_tables >>  refresh_mview_pools_balances
+    create_history_entry = PostgresOperator(
+        task_id="create_history_entry",
+        postgres_conn_id="ton_db",
+        sql=[
+            """
+            create or replace view view_dex_tvl_current
+            as
+            with prices_v1 as (
+              select jetton_a as address, (sum(balance_b) / sum(balance_a)) as price
+              from mview_dex_pools_balances
+              where jetton_b = 'EQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAM9c' 
+                    or jetton_b = 'EQBPAVa6fjMigxsnHF33UQ3auufVrg2Z8lBZTY9R-isfjIFr' -- JTON
+                    or jetton_b = 'EQDQoc5M3Bh8eWFephi9bClhevelbZZvWhkqdo80XuY_0qXv' -- WTON
+                    or jetton_b = 'EQCM3B12QK1e4yZSf8GtBRT0aLMNyEsBc_DhVfRRtOEffLez' -- pTON
+                    or jetton_b = 'EQCajaUU1XXSAjTD-xOV7pE49fGtg4q8kF3ELCOJtGvQFQ2C' -- WTON from megaton
+              group by 1
+            ), prices_v2 as (
+              select jetton_a as address, avg(balance_b / balance_a * prices_v1.price) as price
+              from mview_dex_pools_balances b join prices_v1 on b.jetton_b = prices_v1.address
+              group by 1
+            ), prices_all as (
+              select address, coalesce(prices_v1.price, prices_v2.price) as price from prices_v1
+              full outer join prices_v2 using(address)
+            ),
+            datamart as (
+              select platform, type, b.address, jetton_a, jetton_b ,
+              case 
+                when jetton_b = 'EQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAM9c' 
+                    or jetton_b = 'EQBPAVa6fjMigxsnHF33UQ3auufVrg2Z8lBZTY9R-isfjIFr' -- JTON
+                    or jetton_b = 'EQDQoc5M3Bh8eWFephi9bClhevelbZZvWhkqdo80XuY_0qXv' -- WTON
+                    or jetton_b = 'EQCM3B12QK1e4yZSf8GtBRT0aLMNyEsBc_DhVfRRtOEffLez' -- pTON
+                    or jetton_b = 'EQCajaUU1XXSAjTD-xOV7pE49fGtg4q8kF3ELCOJtGvQFQ2C' -- WTON from megaton
+                  then balance_b * 2
+                when jetton_a = 'EQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAM9c' 
+                    or jetton_a = 'EQBPAVa6fjMigxsnHF33UQ3auufVrg2Z8lBZTY9R-isfjIFr' -- JTON
+                    or jetton_a = 'EQDQoc5M3Bh8eWFephi9bClhevelbZZvWhkqdo80XuY_0qXv' -- WTON
+                    or jetton_a = 'EQCM3B12QK1e4yZSf8GtBRT0aLMNyEsBc_DhVfRRtOEffLez' -- pTON
+                    or jetton_a = 'EQCajaUU1XXSAjTD-xOV7pE49fGtg4q8kF3ELCOJtGvQFQ2C' -- WTON from megaton
+                  then balance_a * 2
+                when p1.price is not null and p2.price is not null
+                  then balance_a * p1.price + balance_b * p2.price
+                when p1.price is not null and p2.price is null
+                  then balance_a * p1.price * 2
+                when p2.price is not null and p1.price is null
+                  then balance_a * p2.price * 2
+                else null
+              end as tvl_ton, 
+              p1.price, p2.price,
+              balance_a * p1.price, balance_b * p2.price
+              from mview_dex_pools_balances b
+              left join prices_all p1 on p1.address = b.jetton_a
+              left join prices_all p2 on p2.address = b.jetton_b
+            ), symbols as (
+             select address, symbol from jetton_master
+             union all
+             select 'EQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAM9c' as address, 'TON' as symbol
+            )
+            select datamart.address, platform, jetton_a, jetton_b,
+            round(tvl_ton / 1000000000) as tvl_ton
+            from datamart
+            where tvl_ton is not null            
+            """,
+            """
+            insert into tvl_history_datamart(build_time, platform, address, jetton_a, jetton_b, tvl_ton)
+            select now(), platform, address, jetton_a, jetton_b, tvl_ton from view_dex_tvl_current            
+            """
+            ]
+    )
+
+    create_tables >>  refresh_mview_pools_balances >> create_history_entry
 
 
 
